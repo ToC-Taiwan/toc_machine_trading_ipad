bef45554f1a6b744b404be3537ef50ca2a6a1cb7
Tim Hsu <maochindada@gmail.com>
2022-12-22 11:47:50 +0800

    fix(common): fix common error

--- lib/global/url.dart
+++ lib/global/url.dart
@@ -2,8 +2,3 @@ const String tradeAgentHost = '172.20.10.228:26670';
 const String tradeAgentURLPrefix = 'http://$tradeAgentHost/tmt/v1';
 const String tradeAgentWSURLPrefix = 'ws://$tradeAgentHost/tmt/v1/stream/ws/pick-stock';
 const String tradeAgentFutureWSURLPrefix = 'ws://$tradeAgentHost/tmt/v1/stream/ws/future';
-
-// const String tradeAgentHost = 'trader.tocraw.com';
-// const String tradeAgentURLPrefix = 'https://$tradeAgentHost/tmt/v1';
-// const String tradeAgentWSURLPrefix = 'wss://$tradeAgentHost/tmt/v1/stream/ws/pick-stock';
-// const String tradeAgentFutureWSURLPrefix = 'wss://$tradeAgentHost/tmt/v1/stream/ws/future';
